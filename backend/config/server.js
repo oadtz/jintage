755aa2a29ce0d488f6dd232c78ad0a3a2ab7e495
Thanapat Pirmphol <t.pirmphol@gmail.com>
2020-12-20 22:01:32 +0700

    no message

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '01d35998607fcef16c8a0ded9863d712'),
+    },
+  },
+});
